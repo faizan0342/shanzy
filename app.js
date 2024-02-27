917382f032ba3df5f70a60b8fbe3a3f63822da69
faizan0342 <faizanshaikh0342@gmail.com>
2024-02-27 06:31:39 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,191 @@
+
+
+
+
+
+// // var elementbtn1 = document.querySelector(".input1");
+// // var elementbtn2 = document.querySelector(".input2");
+// // var elementbtn3 = document.querySelector(".input3");
+
+// // var addlist = document.querySelector(".btn1");
+
+// // var ressult  = document.querySelector(".result")
+
+// // console.log("ressult =====>" , ressult)
+
+
+// // addlist.addEventListener("click", function() {
+// //     var value1 = parseInt(elementbtn1.value);
+// //     var value2 = parseInt(elementbtn2.value);
+// //     var value3 = parseInt(elementbtn3.value);
+
+
+// //     ressult.innerHTML = value1 + value2 + value3 
+// //     // console.log('element ==>', value1 + value2);
+// // });
+
+// // var elementbtn1 = document.querySelector(".input1");
+// // var elementbtn2= document.querySelector(".input2");
+// // var elementbtn3 = document.querySelector(".input3");
+
+// var addlist = document.querySelector(".btn1");
+// var ressult = document.querySelector(".result");
+// console. log("ressult=====> , result");
+// addlist.addEventListener("click" ,function(){
+//     var value1 = parseInt(elementbtn1 . value );
+//     var value2 = parseInt(elementbtn2 . value );
+//     var value3 = parseInt(elementbtn3 . value );
+//     ressult.innerHTML= value1 + value2+ value3
+//     console.log(value1 + value2 + value3)
+// })
+
+
+
+// var elementbtn1 = document.querySelector(".input1");  
+// var elementbtn2 = document.querySelector(".input2");  
+// var elementbtn3 = document.querySelector(".input3");  
+
+
+//     var addlist = document.querySelector(".btn1");
+//    var ressult = document.querySelector(".result");
+//    console.log(".result");
+//      addlist.addEventListener("click" ,function(){
+//     var value1 = parseInt(elementbtn1.value );
+//     var value2 = parseInt(elementbtn2.value );
+//     var value3 = parseInt(elementbtn3.value );
+//     ressult.innerHTM= value1 + value2 + value3
+//     console.log(value1 +value2 + value3);
+// });
+
+/////////////////pp///////
+// function neha(){
+//     return"neha"
+// }
+
+// neha ()
+// var neha ="neha"
+// console.log(neha)
+
+
+// var num = 100
+// for (var i=0;i<=100;i++){
+//     console.log(i)
+// }
+// function sum (a,b){
+//     console.log(a+b)
+// }
+// sum(2,3)
+
+// var arry =[1,2,3,4,5,6,7,]
+// console.log(arry)
+// num.sorts ();
+// num.reverse();
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(arry)
+// fruits.sort();
+// fruits.reverse();
+
+// var arry = ["flower" , "flow" , "flight"]
+
+// function longestCommonPrefix(data){
+//     for(var i = 0 ; i < arry.length ; i++){
+//         for(var j = 1 ; j < arry.length ; j++){
+//             if(arry[0][i] != arry[j][i]){
+//                 return arry[0].slice(0 , i)
+//             }
+//         }
+//     }
+// }
+
+// console.log(longestCommonPrefix(arry))
+
+
+
+// var arry = ["flower", "flow", "flowht"];
+
+// function longestCommonPrefix(arry) {
+
+//     for (var i = 0; i < arry[0].length; i++) {
+//         for (var j = 0; j < arry.length; j++) {
+//             if (arry[0][i] != arry[j][i]) {
+//                 return arry[0].slice(0, i);
+//             }
+//         }
+//     }
+
+// }
+
+// console.log(longestCommonPrefix(arry)); // Output: "fl"
+
+
+// var arry = ["flower" , "flow" , "flight"]
+
+
+// for(var i = 0 ; i < 1 ; i++){
+//     console.log(arry[i][1])
+// }
+//////////////pppp/
+// var arry=["flower","flow","floght"]
+// function longestCommonPrefix(){
+//     for(var i=0;i< arry[0] .length; i++){
+//     for(var j=0;j< arry .length; j++){
+//         if(arry[0][i] != arry[j][i]){
+//             return arry[0] .slice(0,i);
+//         }
+//     }
+//     }
+// }
+
+// console.log (longestCommonPrefix(arry));
+
+
+//  var arry=["flower","flight","flow"]
+
+//  function longestCommonPrefix(){l;
+//     for(var i=0;i< arry[0].length;i++){
+//     for(var j= 0;i< arry.length;j++){
+//         if(arry [0][i] !=[j][i]){
+//             return arry[0]. slice(0,i);
+//         }
+//     }
+//     }
+//  }
+
+// console.log(longestCommonPrefix(arry));
+
+
+
+
+
+
+// var arry =["flower","flow","flog"]
+// function longestCommonPrefix(){
+//         for(var i=0;i<arry[0].length;i++){
+//         for(var j=0;j<arry.length;j++){
+//             if(arry[0][i] !=arry[j][i]){
+//                 return arry[0].slice(0,i);
+//         }
+//     }
+// }
+// }
+// console.log (longestCommonPrefix(arry);
+
+
+
+// var num =prompt("enter the number")
+// if(num /2==0){
+//     console.log("even")
+// }else{
+//     console.log("odd")
+// }
+
+// var arry=[1,2,3,4]
+// console.log(arry)
+// var name = "shanzy"
+// if(name == reversename){
+//     alert("its palindom")
+// }else{
+//     alert("its  not palindom")
+// }
+var name = "neha"
